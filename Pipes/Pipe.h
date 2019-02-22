806dea55e53d7b456af5b245d3d70e147abc4fbd
Dmitry Alexandrovsky <dimi@uni-bremen.de>
2019-02-22 11:10:58 +0100

    Pipes Struct and more

--- Pipes/Pipe.h
+++ Pipes/Pipe.h
@@ -1,4 +1,6 @@
 
+#define NUM_CON = 4
+
 enum eConnectionType {
   North = 0,
   East = 1,
@@ -9,5 +11,33 @@ enum eConnectionType {
 struct Pipe{
  unsigned int id = -1;
  bool visited = false;
- bool connections[4] = {0};
+ bool connections[NUM_CON] = {0};
+
+  public inline RotateLeft(){
+    for(int i = 0; i < NUM_CON; i++){
+      connections[(i-1)%NUM_CON] = connections[i];
+    }
+  }
+
+
+  public inline RotateRight(){
+    for(int i = NUM_CON-1; i >= 0 ; i--){
+      connections[i] = connections[(i-1)%NUM_CON];
+    }
+  }
+ 
+};
+
+
+public class PipesGameFiled {
+
+  Pipe[][] grid;
+  unsigned int width;
+  unsigned int height;
+
+  public PipesGameFiled(int w, int h):width(w), height(h)
+  {
+    grid = Pipe[width][height]; 
+  }
+  
 };
